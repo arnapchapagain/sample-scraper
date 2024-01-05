be3a8699ade2dbc1a63b52255e57155fdbde7e51
Sirshak Bohara <boharasirshak101@gmail.com>
2024-01-05 17:40:03 +0545

    Fix formatting and add missing imports

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass, fields, asdict
 from aiohttp import ClientSession, ClientResponseError
 from bs4 import BeautifulSoup
 
+
 @dataclass
 class AdditionalInfo:
     label: str | list[str]
@@ -27,7 +28,7 @@ class Product:
 def append_to_json(data: Product):
     with open("products.json", "r+", encoding="utf-8") as file:
         try:
-            file.seek(0) # Go to the start of the file
+            file.seek(0)  # Go to the start of the file
             json_data = json.load(file)  # Load existing data
             json_data.append(asdict(data))  # Append new data
 
@@ -40,7 +41,6 @@ def append_to_json(data: Product):
             file.seek(0)
             file.truncate()  # Clear the file before writing
             json.dump([asdict(data)], file, indent=4, ensure_ascii=False)
-            
 
 
 def append_to_csv(product: Product):
@@ -50,7 +50,7 @@ def append_to_csv(product: Product):
         product_dict = asdict(product)
         product_dict.pop("additional")
         writer.writerow(product_dict)
-            
+
 
 async def get_html(client: ClientSession, url: str, **kwargs) -> BeautifulSoup:
     headers = {
@@ -59,7 +59,7 @@ async def get_html(client: ClientSession, url: str, **kwargs) -> BeautifulSoup:
     async with client.get(url, headers=headers, params=kwargs) as request:
         response = await request.text()
         return BeautifulSoup(response, "html.parser")
-        
+
 
 def get_text(element: BeautifulSoup, css_selector: str, default_value: None) -> str:
     try:
@@ -80,31 +80,45 @@ async def scrape_page(client: ClientSession, url: str, **kwargs):
     tasks = []
     product_info = html.select("a.woocommerce-loop-product__link")
     product_links = [product.get("href") for product in product_info]
-        
+
     for product_link in product_links:
         tasks.append(asyncio.ensure_future(get_html(client, product_link)))
-    
+
     for task in asyncio.as_completed(tasks):
         product_page = await task
         product = Product(
-            title = get_text(product_page, "h1.product_title", None),
-            image = get_image(product_page, "div.woocommerce-product-gallery__image > a", None),
-            price = get_text(product_page, "span.amount > bdi", None),
-            short_description = get_text(product_page, "div.woocommerce-product-details__short-description > p", None),
-            description = get_text(product_page, "div#tab-description", None),
-            sku = get_text(product_page, "span.sku", None),
-            cateogory = get_text(product_page, "span.posted_in", None).replace("Category: ", ""),
-            additional = []
+            title=get_text(product_page, "h1.product_title", None),
+            image=get_image(
+                product_page, "div.woocommerce-product-gallery__image > a", None
+            ),
+            price=get_text(product_page, "span.amount > bdi", None),
+            short_description=get_text(
+                product_page,
+                "div.woocommerce-product-details__short-description > p",
+                None,
+            ),
+            description=get_text(product_page, "div#tab-description", None),
+            sku=get_text(product_page, "span.sku", None),
+            cateogory=get_text(product_page, "span.posted_in", None).replace(
+                "Category: ", ""
+            ),
+            additional=[],
         )
-        
+
         tr_tags = product_page.select("table.variations > tbody > tr")
         for tr in tr_tags:
-            product.additional.append(AdditionalInfo(
-                label = tr.select_one("th > label").text,
-                # Excluding the first option "Choose an option"
-                values = [value.text for value in tr.select("td > select > option") if value.text != "Choose an option"]
-            ))
-        
+            product.additional.append(
+                AdditionalInfo(
+                    label=tr.select_one("th > label").text,
+                    # Excluding the first option "Choose an option"
+                    values=[
+                        value.text
+                        for value in tr.select("td > select > option")
+                        if value.text != "Choose an option"
+                    ],
+                )
+            )
+
         yield product
 
 
@@ -112,7 +126,7 @@ async def main():
     url = "https://gopher1.extrkt.com/"
     page = 1
     products = []
-    
+
     async with ClientSession(raise_for_status=True) as client:
         while True:
             try:
@@ -121,21 +135,24 @@ async def main():
                     pprint(product)
                     append_to_json(product)
                     append_to_csv(product)
-                    
+
                 page += 1
-                
+
             except ClientResponseError as e:
                 print("Page limit reached")
                 break
 
+
 if __name__ == "__main__":
     with open("products.json", "w", encoding="utf-8") as file:
         file.write("[]")
-    
+
     with open("products.csv", "w", encoding="utf-8") as file:
-        csv_headers = [header.name for header in fields(Product) if header.name != "additional"] 
+        csv_headers = [
+            header.name for header in fields(Product) if header.name != "additional"
+        ]
         csv_writer = csv.DictWriter(file, fieldnames=csv_headers)
         csv_writer.writeheader()
         del csv_writer
-        
+
     asyncio.run(main())
